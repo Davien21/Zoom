880ed49c304b5a4db16d1b62bb9e5977c67f9bfb
davien21 <chidiebereekennia@gmail.com>
2019-11-06 10:55:51 +0100

    Second

---
+++ js/zoom.js
@@ -0,0 +1,14 @@
+console.log("It's linked!");
+// function sayHello () {
+// 	let anybody = prompt("Who do you wanna say hi to?");
+// 	alert("Hello " + anybody + "!")
+// }
+
+// let firstText = document.querySelector('p');
+// firstText.onclick = () => {
+// 	alert("Congrats, you clicked Me oh!")
+// }
+// let secondText = document.querySelector('span');
+// secondText.onclick = function() {
+// 	sayHello();
+// }
